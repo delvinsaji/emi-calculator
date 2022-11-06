0b8fdb44d7b9f46091fa6155c0c83407fe33ed89
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-11-06 18:03:50 +0530

    emi

--- Pages/EMI.js
+++ Pages/EMI.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { StyleSheet } from "react-native";
 import Globalstyle from "../Global";
 
@@ -11,16 +11,32 @@ export default function EMI() {
       <TextInput></TextInput>
       <Text>Tenor/Duration</Text>
       <TextInput></TextInput>
-      <View>
-        <Button style={[styles.but1, styles.but]} title="CALCULATE"></Button>
-        <Button style={[styles.but2, styles.but]} title="RESET"></Button>
+      <View style={styles.butview}>
+        <TouchableOpacity style={[styles.but1, styles.but]}>
+          <Text>CALCULATE</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.but2, styles.but]}>
+          <Text>RESET</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  but1: {},
-  but2: {},
-  but: {},
+  but1: { backgroundColor: "blue" },
+  but2: { backgroundColor: "grey" },
+  but: {
+    margin: 15,
+    width: 150,
+    alignItems: "center",
+    height: 50,
+    justifyContent: "center",
+    borderRadius: 10,
+  },
+  butview: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
